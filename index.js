c5a40293d39115a57701a27f72ca1fb6f64fbb70
Taavi <taavi.taivere@khk.ee>
2022-02-01 00:28:40 +0200

    post request use

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express')
-const app = express();
+const app = express()
 const path = require('path')
-app.use(express.static('public'));
+
+const bodyParser = require('path')
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('views engine', 'ejs');
-app.get('/questions', (req, res)=> {
-    let questions = [
-        {title: "What is node.js?", user: "kadi", votes: "10"},
-        {title: "What is express.js?", user: "mike", votes: "8"}
-    ]
-    res.render('index', {questions : questions});
+
+app.get('/login', (req, res)=> {
+    res.render('index', 'login');
 });
+app.post('/login',(req, res)=> {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if(username === 'user' && password === 'qwerty'){
+        re.redirect('/dashboard');
+    }
+})
 app.listen(3000, ()=> {
     console.log('Server started on http://localhost:3000');
 });
