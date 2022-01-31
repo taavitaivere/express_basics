2df3c99cacd60f90663a6ed741d941aa750ae9c8
Taavi <taavi.taivere@khk.ee>
2022-01-31 23:43:50 +0200

    template loop use

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const path = require('path')
+const express = require('express')
 const app = express();
-
+const path = require('path')
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('views engine', 'ejs');
-app.get('/user/:username', (req, res)=> {
-    let user = req.params.username;
-    res.render('index', {username : user});
+app.get('/questions', (req, res)=> {
+    let questions = [
+        {title: "What is node.js?", user: "kadi", votes: "10"},
+        {title: "What is express.js?", user: "mike", votes: "8"}
+    ]
+    res.render('index', {questions : questions});
 });
 app.listen(3000, ()=> {
     console.log('Server started on http://localhost:3000');
